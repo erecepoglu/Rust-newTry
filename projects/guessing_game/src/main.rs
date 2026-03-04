1395c22f4614808b2f8464e92e4c205537e933cd
Recepoğlu <73506878+erecepoglu@users.noreply.github.com>
2026-03-04 22:23:06 +0000

    extra editing

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+//use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -7,8 +7,18 @@ fn main() {
     let mut guess = String::new();
     
 
-    io::stdin()
+    std::io::stdin()
         .read_line(&mut guess).expect("Failed to read a line");
     
     println!("You guessed {guess}");
+    let apples = 3;//immutable, also it's immutable by default
+    let mut mut_apples  = 4;
+
+    println!("The immutable apples: {apples}");
+    println!("The mutable apples: {mut_apples}");
+    //apples = 4; it will cause a problem it must be mut apples since we want to change it later
+    mut_apples = 0;
+    println!("The immutable apples: {apples}");
+    println!("The mutable apples: {mut_apples}");
+    //print!();
 }
